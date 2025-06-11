e9d03e4ddd24f3ab493d92400a2500624569c302
Khadija <bsai23006@itu.edu.pk>
2025-06-12 01:07:27 +0500

    add User model with schema and password hashing functionality

---
+++ API/models/User.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose")
+const bcrypt = require("bcryptjs")
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "Please provide a username"],
+    unique: true,
+    trim: true,
+    minlength: [3, "Username must be at least 3 characters long"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide an email"],
+    unique: true,
+    match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, "Please provide a valid email"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+    minlength: [6, "Password must be at least 6 characters long"],
+    select: false,
+  },
+  profilePicture: {
+    type: String,
+    default: "default-avatar.png",
+  },
+  bio: {
+    type: String,
+    default: "",
+  },
+  karma: {
+    type: Number,
+    default: 0,
+  },
+  isVerified: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+})
+
+// Hash password before saving
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next()
+  }
+
+  const salt = await bcrypt.genSalt(10)
+  this.password = await bcrypt.hash(this.password, salt)
+  next()
+})
+
+// Method to check if password matches
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password)
+}
+
+module.exports = mongoose.model("User", UserSchema)
